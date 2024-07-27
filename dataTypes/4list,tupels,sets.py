4723b51c316a0a70b6f5e99af4cdc296ab49f539
m77box <liminyang17@gmail.com>
2024-07-27 16:09:24 -0400

    changes

---
+++ dataTypes/4list,tupels,sets.py
@@ -0,0 +1,12 @@
+age = 46
+apple = 0.87
+courses = ['math', 'english', age, 'piano', 'python', apple, 'pear']
+name = ['Limin','yang']
+prices = [0.73, 0.45, 9.9, 9.4]
+
+courses.extend(name)
+
+# courses.insert(-2, prices)
+
+array = []
+print(courses)
---
+++ dataTypes/basic.py
@@ -0,0 +1,27 @@
+#int
+
+salary = 100000
+print(f"limin's desire salary is ${salary}.")
+
+bag = 10
+# print(f"my total bags is {bag}")
+
+
+#float
+
+apple_price_per_pound = 0.51000
+# print(f"the apple is {apple_price_per_pound} per pound")
+print(apple_price_per_pound+bag)
+#float to int
+
+
+#boolean
+
+online = False
+print('the work is online {online}')
+# print(f"the work is onsite {onsite}")
+
+# if online:
+#     print("the work is online")
+# else:
+#     print("the work is onsite")
