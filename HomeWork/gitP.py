3060059fd1fd7b24653a99f30f33c61664ddac77
m77box <liminyang17@gmail.com>
2024-08-18 19:16:04 -0400

    add a new update

---
+++ HomeWork/gitP.py
@@ -0,0 +1,8 @@
+# Given two dictionaries dict1 = {'a': 1, 'b': 2} and dict2 = {'b': 3, 'c': 4}, how do you merge them into one dictionary?
+# Common Errors:
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}
+
+d8 = dict2 | dict1
+print(d8)
