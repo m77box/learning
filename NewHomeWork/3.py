18fc63cdd42a159ffda28d2defadb63a38922c7c
m77box <liminyang17@gmail.com>
2024-08-22 22:57:21 -0400

    I M so smart

--- NewHomeWork/3.py
+++ NewHomeWork/3.py
@@ -2,15 +2,20 @@
 # Problem: Write a function second_largest(lst) that takes a list of numbers and returns the second largest number.
 
 def second_largest(lst):
-    l = {}
-    for i in range(1, lst):
-        i.append()
+    # print(lst)
+    l1 = {}
+    num = 0
+    new_l1 = sorted(lst, reverse=False)
+    n =len(new_l1)-2
+    return new_l1[n]
+
+
 
 
 # # How to test:
 # print(second_largest(l1))  # Expected output: 4
 
-num = {'9', '5', '4', '5'}
+num = {9, 5, 4, 5,6,11,12,13}
 print(second_largest(num))
 
 # # How to test:
--- NewHomeWork/5.py
+++ NewHomeWork/5.py
@@ -1,22 +1,48 @@
 # Question 5: Calculate Factorial Using Recursion
 # Problem: Write a function factorial(n) that takes an integer n and returns its factorial using recursion.
 
-def factorial(n):
-    if n <=0:
-        print("done")
-    else:
-        print(n)
-        x = factorial((n-1))
-    return x
 
-y = 8
+# how many ways to solve problem, there are 5000 ways to solve problem
+def factorial(n):
+    l =[]
+    while n>0:
+        l.append(n)
+        n = n - 1
+        print(n)
+    total = 1
+    for i in l:
+        total = total * i
+    return total
+
+
+
+y = 5
 # How to test:
 print(factorial(y))  # Expected output: 120
-
-
 
 # print(factorial(3))  # Expected output: 6
 
 
 # Also there are hard questions if you get dont with this
 # BEST OF LUCK!
+
+# limin = []
+# print(limin)
+# limin.append('string called smart limin')
+# print(limin)
+# limin.append('string called not so smart limin')
+# print(limin)
+# limin.append(6)
+# print(limin)
+# limin.append('6')
+# print(limin)
+# rutvik = ['not','so','smart']
+# limin.append(rutvik)
+# print(limin)
+# rutvik = {'not':'so'}
+# limin.append(rutvik)
+# print(limin)
+# for i in limin:
+#     print(i)
+#     print(type(i))
+#
