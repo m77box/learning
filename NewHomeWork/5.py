89cf09730bca7770457f1ef71eacb0c734bf47bd
m77box <liminyang17@gmail.com>
2024-08-22 20:20:57 -0400

    se

--- NewHomeWork/5.py
+++ NewHomeWork/5.py
@@ -1,18 +1,17 @@
 # Question 5: Calculate Factorial Using Recursion
 # Problem: Write a function factorial(n) that takes an integer n and returns its factorial using recursion.
 
-# def factorial(n):
-#     x=0
-#     if n <=0:
-#         print("done")
-#     else:
-#         print(n)
-#         x = factorial(n-1)
-#     return x
-#
-# y = 5
-# # How to test:
-# print(factorial(y))  # Expected output: 120
+def factorial(n):
+    if n <=0:
+        print("done")
+    else:
+        print(n)
+        x = factorial((n-1))
+    return x
+
+y = 8
+# How to test:
+print(factorial(y))  # Expected output: 120
 
 
 
@@ -21,16 +20,3 @@
 
 # Also there are hard questions if you get dont with this
 # BEST OF LUCK!
-
-def factorial(n):
-    if n == 0:  # Base case: factorial of 0 is 1
-        return 1
-    else:
-        return n * factorial(n-1)  # Recursive case: n * factorial of n-1
-
-y = 5
-# How to test:
-print(factorial(y))  # Expected output: 120
-
-print(factorial(3))  # Expected output: 6
-
