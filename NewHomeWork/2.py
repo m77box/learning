a83ed1f9fff270334922ddbb6c8220889d1499f0
Rutvik Patel <rutvik2611@gmail.com>
2024-08-25 20:44:05 -0400

    Revert "update"

--- NewHomeWork/2.py
+++ NewHomeWork/2.py
@@ -1,16 +1,10 @@
 # Question 2: Check if a Number is Prime
 # Problem: Write a function is_prime(n) that takes an integer n and returns True if the number is prime, and False otherwise.
 
-def is_prime(num):
-    if num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
-                return False
+def is_prime(n):
+    # Your code will go here
+    pass
 
-        return True
-    else:
-        print("the number needs to be greater than 1.")
-
-x = int(input("enter any number: "))
 # How to test:
-print(is_prime(x))   # Expected output: True
+print(is_prime(7))   # Expected output: True
+print(is_prime(10))  # Expected output: False
